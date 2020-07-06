3d33192484b4587ee29caf624b383c7e167598d2
joeluca24 <65137591+joeluca24@users.noreply.github.com>
2020-07-06 14:02:55 -0400

    added the correct total of questions

---
+++ README.md
@@ -0,0 +1,2 @@
+# Question Trivia Project
+
--- main.js
+++ main.js
@@ -13,7 +13,7 @@ var interval;
 function startTimer() {
     document.getElementById("answer-buttons").style.display = "block"; // turn on the display
     document.getElementById("start").style.display = "none"; // turn off the display 
-    var minutes = 10; 
+    var minutes = 10;
     totalSeconds = minutes * 60;
     secondsElapsed = 0;
     interval = setInterval(runClockCb, 1000);
@@ -23,8 +23,8 @@ function runClockCb() {
     secondsElapsed++;
     minutesDisplay.textContent = Math.floor((totalSeconds - secondsElapsed) / 60);
     secondsDisplay.textContent = (totalSeconds - secondsElapsed) % 60;
-    console.log({totalSeconds, secondsElapsed});
-    
+    // console.log({totalSeconds, secondsElapsed});
+
     // this means the timer will stop at zero.
     if (totalSeconds <= secondsElapsed) {
         (clearInterval(interval));
@@ -55,7 +55,81 @@ var questionList = [
         "d": "undefined.",
         "correct": "a",
         "userAnswer": null
+    },
+    {
+        "question": "how many days are in a year?",
+        "a": "365.",
+        "b": "478.",
+        "c": "12.",
+        "d": "389.",
+        "correct": "a",
+        "userAnswer": null
 
+    },
+    {
+        "question": "who is the first president of the usa?",
+        "a": "george lopez.",
+        "b": "john adams.",
+        "c": "George washington.",
+        "d": "freddie mercury",
+        "correct": "c",
+        "userAnswer": null
+        
+    },
+    {
+        "question": "How many states make up the usa?",
+        "a": "52.",
+        "b": "50.",
+        "c": "124.",
+        "d": "24.",
+        "correct": "b",
+        "userAnswer": null
+
+    },
+    {
+        "question": "How many feet are in a yard ?",
+        "a": "7 ft.",
+        "b": "1ft.",
+        "c": "13ft.",
+        "d": "3ft.",
+        "correct": "d",
+        "userAnswer": null
+    },
+    {
+        "question": "how many keys are on a classic piano?",
+        "a": "88.",
+        "b": "144.",
+        "c": "52.",
+        "d": "68.",
+        "correct": "a",
+        "userAnswer": null
+    },
+    {
+        "question": "What is the capital of Florida?",
+        "a": "miami.",
+        "b": "davie.",
+        "c": "tallahasse.",
+        "d": "tampa.",
+        "correct": "c",
+        "userAnswer": null
+    },
+    {
+        "question": "how fast can a chettah run ?",
+        "a": "58mph.",
+        "b": "100mph.",
+        "c": "32mph.",
+        "d": "68mph.",
+        "correct": "a",
+        "userAnswer": null
+    },
+    {
+        "question": "who is the coolest team inn the AFC east?",
+        "a": "jets.",
+        "b": "bills.",
+        "c": "dolphins",
+        "d": "patriots",
+        "correct": "a",
+        "userAnswer": null
     }
 ];
 
@@ -74,30 +148,51 @@ var score = document.body.querySelector("#score");
 
 var questionIndex = 0;
 
+// This function handles the button click
 function buttonHandler(event) {
     var button = event.target;
     var userAnswer = button.getAttribute("data-answer");
     var questionId = parseInt(button.getAttribute("data-question"));
-    console.log(button);
-    console.log(userAnswer);
-    console.log(questionId);
+    // console.log("buttonHandler");
+    // console.log(button);
+    // console.log(userAnswer);
+    // console.log(questionId);
     questionList[questionId]["userAnswer"] = userAnswer;
 
     if (questionList[questionId]["userAnswer"] === questionList[questionId]["correct"]) {
         score.textContent = "You got it correct";
-        setTimeout(function () {
-            questionIndex++;
-            initializeQuestion();
-            score.textContent = "";
-        }, 5000);
+        questionIndex++;
+        // endGame condition
+        if (questionIndex >= questionList.length) {
+            console.log("calculate score and stop timer");
+            clearInterval(interval);
+            calculateScore();
+        }
+        else {
+            setTimeout(function () {
+
+                initializeQuestion();
+                score.textContent = "";
+
+            }, 5000);
+        }
     }
     else {
         score.textContent = "You got it wrong";
-        setTimeout(function () {
-            questionIndex++;
-            initializeQuestion();
-            score.textContent = "";
-        }, 5000);
+        questionIndex++;
+        // endGame condition
+        if (questionIndex >= questionList.length) {
+            console.log("calculate score and stop timer");
+            clearInterval(interval);
+            calculateScore();
+        }
+        else {
+
+            setTimeout(function () {
+                initializeQuestion();
+                score.textContent = "";
+            }, 5000);
+        }
     }
 }
 // // THEN the game is over
@@ -107,6 +202,12 @@ buttonA.addEventListener("click", buttonHandler);
 buttonB.addEventListener("click", buttonHandler);
 buttonC.addEventListener("click", buttonHandler);
 buttonD.addEventListener("click", buttonHandler);
+
+function calculateScore() {
+    document.querySelector("ul").style.display = 'none';
+    document.querySelector("#question").style.display = 'none';
+    document.querySelector("#score").textContent = "You have a score of 1 of 10";
+}   userScore = userScore + 1;
 
 function initializeQuestion() {
     console.log(questionList[questionIndex]);
@@ -126,4 +227,6 @@ function initializeQuestion() {
     buttonD.setAttribute("data-question", questionIndex);
 }
 initializeQuestion();
+
+
 // THEN I am presented with another question
